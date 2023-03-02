61a17dc606c7800ed20362ae93d01f62e51a4b82
Aleksey Likhtinov <91214571+Grec88@users.noreply.github.com>
2023-03-02 11:06:10 +0300

    Timer

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -2,3 +2,4 @@ export { modals } from './modals.js';
 export { tabs } from './tabs.js';
 export { form } from './form.js';
 export { changeModalState } from './changeModalState.js';
+export { timer } from './timer.js'
---
+++ src/js/modules/timer.js
@@ -0,0 +1,62 @@
+export const timer = (id, deadline) => {
+
+    const addZero = (num) => {
+        if (num <= 9) {
+            return '0' + num;
+        }
+        else {
+            return num;
+        }
+    };
+
+    const getTimeRemaining = (endtime) => {
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        const seconds = Math.floor((t / 1000) % 60);
+        const minutes = Math.floor(((t / 1000) / 60) % 60);
+        const hours = Math.floor(((t / 1000) / 3600) % 24);
+        const days = Math.floor((t / 1000) / 86400);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    };
+    const setClock = (selector, endtime) => {
+        const timer = document.querySelector(selector);
+        const days = timer.querySelector("#days");
+        const hours = timer.querySelector("#hours");
+        const minutes = timer.querySelector("#minutes");
+        const seconds = timer.querySelector("#seconds");
+
+        const updateClock = () => {
+            const t = getTimeRemaining(endtime);
+
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+
+                days.textContent = "00";
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+
+                clearInterval(timeInterval);
+            };
+        };
+
+        const timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+
+    };
+
+    setClock(id, deadline);
+
+};
